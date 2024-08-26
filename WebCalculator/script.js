308b7ddeb604cdf1041f9af70a3d1731345307dc
Mutton9558 <69904687+Mutton9558@users.noreply.github.com>
2024-08-26 10:35:24 +0800

    Dark Mode/Light Mode toggle

--- WebCalculator/script.js
+++ WebCalculator/script.js
@@ -1,3 +1,36 @@
 function showProgress(){
     document.querySelector("#progress-text").innerHTML = "👷‍♂️🚧 In progress 👷‍♂️🚧";
-}
+};
+
+function changeMode(){
+    const MODEBTN = document.querySelector("#btn-mode");
+    if (MODEBTN.innerHTML == "Toggle Dark Mode"){
+        document.querySelector("#color-mode").innerHTML = `
+        * {
+            font-family: 'Roboto Mono';
+            font-weight: 400;
+            color: #e7f1f0;
+            background-color: #23242a;
+
+            margin:0;
+            padding:0;
+        }
+        `;
+        MODEBTN.innerHTML = "Toggle Light Mode";
+    } else if (MODEBTN.innerHTML == "Toggle Light Mode"){
+        document.querySelector("#color-mode").innerHTML = `
+        * {
+            font-family: 'Roboto Mono';
+            font-weight: 400;
+            color: #e8e6fb;
+            background-color: #9287ec;
+
+            margin:0;
+            padding:0;
+        }
+        `;
+        MODEBTN.innerHTML = "Toggle Dark Mode";
+    } else {
+        console.log("Error")
+    };
+};
